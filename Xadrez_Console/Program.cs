351a73ffae86bafcf738efcbbdcc19ade64e4308
Caio Mendes <caio.mendes@autocom-mg.com.br>
2022-02-10 09:50:38 -0300

    Adicionado Posicao Xadrez

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -8,19 +8,9 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 10));
-                Tela.imprimirTabuleiro(tab);
-                
-            }
-            catch (TabuleiroException e )
-            {
-
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
             Console.ReadLine();
         }
     }
